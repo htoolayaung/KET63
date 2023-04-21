3cb0aff67d2894cdd8b8360d129fa70ce9bae7ad
htoolayaung <106012748+htoolayaung@users.noreply.github.com>
2023-04-21 22:45:43 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const button = document.getElementById('redirect-button');
+button.addEventListener('click', function() {
+	window.location.href='https://drive.google.com/file/d/1hiQmjhB9YcYT-6bYsitWPUtSIDf-AN_k/view?usp=sharing';
+});
